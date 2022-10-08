2aff23cfabee8596ed88680b4e8b29da892446d8
uk1996 <csuk1947@gmail.com>
2022-10-08 18:38:52 +0900

    update Request-Body

---
+++ Tutorial/4_Request-Body/main.py
@@ -0,0 +1,10 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
