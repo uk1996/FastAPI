72853770aa799c1fcb385829ed8965a82ca57787
uk1996 <csuk1947@gmail.com>
2022-10-10 21:56:32 +0900

    update 4_Request-Body

--- Tutorial/4_Request-Body/main.py
+++ Tutorial/4_Request-Body/main.py
@@ -4,7 +4,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 
 class Item(BaseModel):
-    name: str
+    name: str = "choi"
     description: Union[str, None] = None
     price: float
     tax: Union[float, None] = None
+
+app = FastAPI()
+
+@app.post("/items/{item_id}")
+async def create_item(item: Item, item_id: int, q: Union[str, None] = None):
+    print(f"data_dict: {item.dict()}")
+    print(f"name: {item.name}")
+    print(f"description: {item.description}")
+    print(f"price: {item.price}")
+    print(f"tax: {item.tax}")
+
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+
+    return result
