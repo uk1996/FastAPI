9255f0ed20506f734ebd24945cc05d95930c61ed
최성욱 <94329093+uk1996@users.noreply.github.com>
2022-10-12 21:14:51 +0900

    오타 수정

--- Tutorial/5_Query Parameters and String Validations/main.py
+++ Tutorial/5_Query Parameters and String Validations/main.py
@@ -11,9 +11,9 @@ async def read_items(q: Union[List[str], None] = Query(
     title="Query string",
     description="Query string for the items to search in the database that have a good match",
     min_length=3, max_length=50, # q의 최소길이 3, 최대 길이를 50자로 지정
-    alias="item_query", # 벌칭 설정 가능
+    alias="item_query", # 별칭 설정 가능
     deprecated=True, # 파라미터를 사용할수는 있지만 추천하지 않을때
-   # include_in_schema=False, # 파라미터를 사용하지 않을때, 사용 불가
+   # include_in_schema=False, # 파라미터를 사용하지 않을때(아예 사용 불가)
     #regex="^a" # 정규표현식(a로 시작)
 )):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
